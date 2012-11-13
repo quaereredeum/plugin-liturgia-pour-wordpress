30a9ef69593f370fa2270f71ce4baa456c782047
Societas laudis <contact@societaslaudis.org>
2012-11-13 08:41:15 +0100

    Tierce

--- tierce.php
+++ tierce.php
@@ -1,334 +1,65 @@
 <?php
 
-global $baseurl;
+function tierce($date,$ordo) {
+$lang=$_GET['lang'];
+$option=$_GET['option'];
+if($lang=="") $lang="fr";
+$xml=$GLOBALS['liturgia'];
+$prio=$xml->xpath("//ordo[@id='RE']//priorite");
+$priorite=$prio[0];
 
-// Activer le rapport d'erreurs PHP
-if ($_SERVER['SERVER_NAME'] == "localhost" )  {
-	//error_reporting(E_ALL);  
-	$baseurl="http://localhost/societaslaudis";
-	}
-	else $baseurl="http://".$_SERVER['SERVER_NAME'];
+//////// INTITULE
+print intitule_matin();
 
-/*
-function tierce($jour,$tableau,$calendarium) {
-	/*
-if(!$my->email) {
-	print"<center><i>Le textes des offices latin/français ne sont disponibles que pour les utilisateurs enregistrés. <a href=\"index.php?option=com_registration&task=register\">Enregistrez-vous ici pour continuer (simple et gratuit)</a>.</i></center>";
-	return;
-} 
-//print"<br>tableau :";
-
-
-$psautier=$tableau['matin']['psautier'];
-$fp = @fopen ("calendrier/liturgia/psautier/".$psautier.".csv","r","1");
-	while ($data = @fgetcsv ($fp, 1000, ";")) {
-	    $id=$data[0];
-	    $reference[$id]['latin']=$data[1];
-	    $reference[$id]['francais']=$data[2];
-	    $row++;
-	}
-	@fclose($fp);
-
-$ferie=$tableau['matin']['ferie'];
-//print_r($tableau['matin']['ferie']);
-$fp = @fopen ("calendrier/liturgia/psautier/".$ferie.".csv","r","1");
-	while ($data = @fgetcsv ($fp, 1000, ";")) {
-	    $id=$data[0];
-	    $reference[$id]['latin']=$data[1];
-	    $reference[$id]['francais']=$data[2];
-	    $row++;
-	}
-	@fclose($fp);
-
-$special=$tableau['matin']['special'];
-$fp = @fopen ("calendrier/liturgia/psautier/".$special.".csv","r","1");
-	while ($data = @fgetcsv ($fp, 1000, ";")) {
-	    $id=$data[0];
-	    $reference[$id]['latin']=$data[1];
-	    $reference[$id]['francais']=$data[2];
-	    $row++;
-	}
-	@fclose($fp);
-
-$propre=$tableau['matin']['propre'];
-$fp = @fopen ("calendrier/liturgia/psautier/".$propre.".csv","r","1");
-	while ($data = @fgetcsv ($fp, 1000, ";")) {
-	    $id=$data[0];
-		$reference[$id]['latin']=$data[1];
-	    $reference[$id]['francais']=$data[2];
-	    $row++;
-	}
-	@fclose($fp);
-
-
-$jours_l = array("Dominica,", "Feria secunda,","Feria tertia,","Feria quarta,","Feria quinta,","Feria sexta,", "Sabbato,");
-$jours_fr=array("Le Dimanche","Le Lundi","Le Mardi","Le Mercredi","Le Jeudi","Le Vendredi","Le Samedi");
-
-$anno=substr($jour,0,4);
-$mense=substr($jour,4,2);
-$die=substr($jour,6,2);
-$day=mktime(12,0,0,$mense,$die,$anno);
-$jrdelasemaine=date("w",$day);
-//print " <br>jrdelasemaine : $jrdelasemaine";
-$date_fr=$jours_fr[$jrdelasemaine];
-$date_l=$jours_l[$jrdelasemaine];
-/*
-if($calendarium['rang'][$jour]) {
-	    $prop=$mense.$die;
-	    $fp = @fopen ("calendrier/liturgia/psautier/".$prop.".csv","r");
-		while ($data = @fgetcsv ($fp, 1000, ";")) {
-	    $id=$data[0];
-	    $propre[$id]['latin']=$data[1];
-	    $propre[$id]['francais']=$data[2];
-	    $row++;
-		}
-	@fclose($fp);
-	}
-*/
-
-$fp = @fopen ("calendrier/liturgia/jours.csv","r","1");
-	while ($data = fgetcsv ($fp, 1000, ";")) {
-	    $id=$data[0];$latin=$data[1];$francais=$data[2];
-	    $jo[$id]['latin']=$latin;
-	    $jo[$id]['francais']=$francais;
-	    $row++;
-	}
-	fclose($fp);
-
-$jrdelasemaine++; // pour avoir dimanche=1 etc...
-
-
-	$row = 0;
-	$fp = @fopen ("calendrier/liturgia/tierce.csv","r","1");
-	while ($data = fgetcsv ($fp, 1000, ";")) {
-	    $latin=$data[0];$francais=$data[1];
-	    $lau[$row]['latin']=$latin;
-	    $lau[$row]['francais']=$francais;
-	    $row++;
-
-	}
-	fclose($fp);
-
-$tem=$tableau['matin']['temps'];
- 	$max=$row;
-	$tierce="
-	<table bgcolor=#FEFEFE>";
-	for($row=0;$row<$max;$row++){
-	    $lat=$lau[$row]['latin'];
-	    $fr=$lau[$row]['francais'];
-	    if(($tem=="Tempus Quadragesimae")&&($lat=="Allelúia.")) {
-			$lat="";
-			$fr="";
-			}
-		if(($tem=="Tempus passionis")&&($lat=="Allelúia.")) {
-		    $lat="";
-			$fr="";
-		}
-
-	if($lat=="#JOUR") {
-	    if($reference['intitule']['latin']){
-	    	$tierce.="<tr><td width=49%><center><b>{$reference['jour']['latin']}</b></center></td>";  
-        $tierce.="<td width=49%><center><b>{$reference['jour']['francais']}</b></center></td></tr>";
-        $tierce.="<tr><td width=49%><center><b>{$reference['intitule']['latin']}</b></center></td>";  
-        $tierce.="<td width=49%><center><b>{$reference['intitule']['francais']}</b></center></td></tr>";
-        $tierce.="<tr><td width=49%><center><font color=red> {$reference['rang']['latin']}</font></center></td><td width=49%><center><font color=red>{$reference['rang']['francais']}</font></center></td></tr>";
-    $tierce.="<tr><td width=49%><center><font color=red><b>Ad Tertiam.</b></font></center></td>";
-		$tierce.="<td width=49%><b><center><font color=red><b>A Tierce.</b></font></center></td></tr>";
-		}
-  		else {
-  		$l=$jo[$jrdelasemaine]['latin'];
-	    $f=$jo[$jrdelasemaine]['francais'];
-		$tierce.="<tr><td width=49%><center><font color=red><b>$date_l ad Tertiam.</b></font></center></td>";
-		$tierce.="<td td width=49%><b><center><font color=red><b>$date_fr à Tierce.</b></font></center></td></tr>";
-			}
-	}
-
-
-	elseif($lat=="#HYMNUS_tertiam") {
-
-	    //$mediahora.=hymne("hy_Ætérne_rerum_cónditor");
-	    $hymne3=$reference['HYMNUS_tertiam']['latin'];
-	    $tierce.=hymne($hymne3);
-
-	    //$row++;
-	}
-
-
-	elseif($lat=="#ANT1*"){
-	
-
-
-	    $antlat=$reference['ant4']['latin'];
-	    $antfr=$reference['ant4']['francais'];
-	
-	    $tierce.="
-	    <tr>
-	<td id=\"colgauche\"><font color=red>Ant. 1 </font> $antlat</td><td id=\"coldroite\"><font color=red>Ant. 1 </font> $antfr</td></tr>";
-	    //$row++;
-
-	}
-
-	elseif($lat=="#PS1"){
-	    $psaume="ps119";
-	    $tierce.=psaume($psaume);
-	    //$row++;
-	}
-
-	elseif($lat=="#ANT1"){
-
-	    
-	
-	    $antlat=$reference['ant4']['latin'];
-	    $antfr=$reference['ant4']['francais'];
-	
-	    $tierce.="
-	    <tr>
-	<td id=\"colgauche\"><font color=red>Ant. </font>$antlat</td><td id=\"coldroite\"><font color=red>Ant. </font>$antfr</td></tr>";
-	    //$row++;
-
-	}
-
-	elseif($lat=="#ANT2*"){
-	   
-	    $antlat=$reference['ant5']['latin'];
-	    $antfr=$reference['ant5']['francais'];
-
-	    $tierce.="
-	    <tr>
-	<td id=\"colgauche\"><font color=red>Ant. 2 </font> $antlat</td><td id=\"coldroite\"><font color=red>Ant. 2 </font> $antfr</td></tr>";
-	    //$row++;
-
-	}
-
-	elseif($lat=="#PS2"){
-	    $psaume="ps120";
-	    $tierce.=psaume($psaume);
-	    //$row++;
-	}
-
- 	elseif($lat=="#ANT2"){
- 	    
-	    $antlat=$$reference['ant4']['latin'];
-	    $antfr=$$reference['ant4']['francais'];
-
-	    $tierce.="
-	    <tr>
-	<td id=\"colgauche\"><font color=red>Ant. </font>$antlat</td><td id=\"coldroite\"><font color=red>Ant. </font> $antfr</td></tr>";
-	    //$row++;
-
-	}
-
-	elseif($lat=="#ANT3*"){
-	    
-	    $antlat=$$reference['ant6']['latin'];
-	    $antfr=$reference['ant6']['francais'];
-
-	    $tierce.="
-	    <tr>
-	<td id=\"colgauche\"><font color=red>Ant. 3</font> $antlat</td><td id=\"coldroite\"><font color=red>Ant. 3</font> $antfr</td></tr>";
-	    //$row++;
-
-	}
-
-	elseif($lat=="#PS3"){
-	    $psaume="ps121";
-	    $tierce.=psaume($psaume);
-	    //$row++;
-	}
-
- 	elseif($lat=="#ANT3"){
- 	    
-	    $antlat=$reference['ant6']['latin'];
-	    $antfr=$reference['ant6']['francais'];
-
-	    $tierce.="
-	    <tr>
-	<td id=\"colgauche\"><font color=red>Ant. </font>$antlat</td><td id=\"coldroite\"><font color=red>Ant. </font> $antfr</td></tr>";
-	    //$row++;
-
-	}
-
-
- 	elseif($lat=="#LB_3"){
-	    
-	    $lectiobrevis=$reference['LB_3']['latin'];
-	    $tierce.=lectiobrevis($lectiobrevis);
-	}
-	elseif($lat=="#RB_3"){
-	    
-	    $rblat=nl2br($reference['RB_3']['latin']);
-	    $rbfr=nl2br($reference['RB_3']['francais']);
-
-	    $tierce.="<tr><td id=\"colgauche\">$rblat</td><td id=\"coldroite\">$rbfr</td></tr>";
-
-	    //$row++;
-		//$laudes.=respbrevis("resp_breve_Christe_Fili_Dei_vivi");
-	}
-
-
-
-
-	elseif($lat=="#ORATIO_3"){
-	    
-	        $oratio3lat=$reference['oratio']['latin'];
-	    	$oratio3fr=$reference['oratio']['francais'];
-	   
-	    if(($reference['oratio_3']['latin'])&&($reference['rang']==""))$oratio3lat=$reference['oratio_3']['latin'];
-	    if(($reference['oratio_3']['latin'])&&($reference['rang']==""))$oratio3fr=$reference['oratio_3']['francais'];
-
-	    if ((substr($oratio3lat,-13))==" Per Dóminum.") {
-	        $oratio3lat=str_replace(" Per Dóminum.", " Per Christum Dóminum nostrum.",$oratio3lat);
-	    	$oratio3fr.=" Par le Christ, notre Seigneur.";
-	    }
-	    
-	    if ((substr($oratio3lat,-14))==" Per Christum.") {
-	        $oratio3lat=str_replace(" Per Christum.", " Per Christum Dóminum nostrum.",$oratio3lat);
-	    	$oratio3fr.=" Par le Christ, notre Seigneur.";
-	    }
-
-        if ((substr($oratio3lat,-11))==" Qui tecum.") {
-	        $oratio3lat=str_replace(" Qui tecum.", " Qui vivit et regnat in sæcula sæculórum.",$oratio3lat);
-	    	$oratio3fr.=" Lui qui vit et règne pour les siècles des siècles.";
-	    }
-
-        if ((substr($oratio3lat,-11))==" Qui vivit.") {
-	        $oratio3lat=str_replace(" Qui vivit.", " Qui vivit et regnat in sæcula sæculórum.",$oratio3lat);
-	    	$oratio3fr.=" Lui qui vit et règne pour les siècles des siècles.";
-	    }
-
-        if ((substr($oratio3lat,-11))==" Qui vivis.") {
-	        $oratio3lat=str_replace(" Qui vivis.", " Qui vivis et regnas in sæcula sæculórum.",$oratio3lat);
-	    	$oratio3fr.=" Toi qui vis et règnes pour les siècles des siècles.";
-	    }
-
-	    
-	    
-	    
-	    $tierce.="
-	    <tr>
-	    <td>Oremus</td><td>Prions</td></tr>
-	    <tr>
-	<td id=\"colgauche\">$oratio3lat</td><td id=\"coldroite\">$oratio3fr</td></tr>";
-	    //$row++;
-	}
-
-
-
-	else {
-	    if(($lat)||($fr))
-		$tierce.="
-		<tr>
-		<td id=\"colgauche\">$lat</td><td id=\"coldroite\">$fr</td></tr>";
-		}
-	}
-	$tierce.="</table>";
-	$tierce= rougis_verset ($tierce);
-
-   $tierce=utf($tierce);
-
-	return $tierce;
-
+$of=$xml->xpath("//ordo[@id='RE']//office_soir/la");
+if($of[0]) $off=$of[0];
+if($off) {
+	print"<div class=\"gauche\"><center><i>".$off."</i></center></div>";
+	print"<div class=\"droite\"><center><i>".get_traduction($off,$lang)."</i></center></div>";
 }
-*/
+else print"<div class=\"gauche\"><center><i>Ad Tertiam</i></center></div><div class=\"droite\"><center><i>".get_traduction("Ad Tertiam", $lang)."</i></center></div>";
 
+// Initial
+print affiche_texte("initial_GHeure",$lang);
+// Hymne
+$hr=$xml->xpath("ordo[@id='RE']/HYMNUS_3");
+$hrr=$hr[0];
+print hymne($hrr,$lang);
+// ant1 et  PS1
+$ar=$xml->xpath("ordo[@id='RE']/ant4");
+$arr=$ar[0];
+print antienne($arr,$lang,"1");
+$pr=$xml->xpath("ordo[@id='RE']/ps4");
+$prr=$pr[0];
+print psaume($prr,$lang);
+print antienne($arr,$lang);
+// ant2 et  PS2
+$ar=$xml->xpath("ordo[@id='RE']/ant5");
+$arr=$ar[0];
+if($arr) print antienne($arr,$lang,"2");
+$pr=$xml->xpath("ordo[@id='RE']/ps5");
+$prr=$pr[0];
+if($prr) print psaume($prr,$lang);
+if($arr) print antienne($arr,$lang);
+// ant3 et  PS3
+$ar=$xml->xpath("ordo[@id='RE']/ant6");
+$arr=$ar[0];
+if($arr) print antienne($arr,$lang,"3");
+$pr=$xml->xpath("ordo[@id='RE']/ps6");
+$prr=$pr[0];
+if($prr) print psaume($prr,$lang);
+if($arr) print antienne($arr,$lang);
+// Lecon et répons bref
+$lr=$xml->xpath("ordo[@id='RE']/LB_3");
+$lrr=$lr[0];
+print lectiobrevis($lrr,$lang);
+$rr=$xml->xpath("ordo[@id='RE']/RB_6");
+$rrr=$rr[0];
+print reponsbref($rrr,$lang);
+
+$oratio=$xml->xpath("ordo[@id='RE']/oratio_3");
+//print_r($oratio);
+print collecte($oratio[0],$lang);
+print affiche_texte("Benedicamus_Domino",$lang);
+}
 ?>
